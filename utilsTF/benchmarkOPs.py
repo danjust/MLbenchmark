713bc6f7a85eb24c076f04c6aa775aa6ee8d9c7e
danjust <daju85@gmail.com>
2017-12-19 12:39:15 +0000

    formatting

--- utilsTF/benchmarkOPs.py
+++ utilsTF/benchmarkOPs.py
@@ -3,20 +3,20 @@ import time
 
 
 def Benchmark_MatMul(n,iterations,dev):
-
     with tf.device(dev):
         matA = tf.Variable(tf.ones([n,n],dtype=tf.float32))
         matB = tf.Variable(tf.ones([n,n],dtype=tf.float32))
         prod = tf.matmul(matA,matB)
 
-    # Creates a session with log_device_placement set to True.
-    # sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    # Creates the session
     config = tf.ConfigProto(graph_options=tf.GraphOptions(optimizer_options=tf.OptimizerOptions(opt_level=tf.OptimizerOptions.L0)))
     sess = tf.Session(config=config)
     sess.run(tf.global_variables_initializer())
 
-    # Runs the op.
+    # Warm-up run
     sess.run(prod.op)
+
+    # Benchmark run
     t = time.time()
     for _ in range(iterations):
         sess.run(prod.op)
@@ -24,20 +24,20 @@ def Benchmark_MatMul(n,iterations,dev):
 
 
 def Benchmark_Conv(n,kernelsize,iterations,dev):
-
     with tf.device(dev):
         matA = tf.Variable(tf.ones([1,n,n,1],dtype=tf.float32))
         kernel = tf.Variable(tf.ones([kernelsize,kernelsize,1,1],dtype=tf.float32))
         conv = tf.nn.conv2d(input=matA,filter=kernel,strides=[1,1,1,1],padding="SAME")
 
-    # Creates a session with log_device_placement set to True.
-    # sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    # Creates the session
     config = tf.ConfigProto(graph_options=tf.GraphOptions(optimizer_options=tf.OptimizerOptions(opt_level=tf.OptimizerOptions.L0)))
     sess = tf.Session(config=config)
     sess.run(tf.global_variables_initializer())
 
-    # Runs the op.
+    # Warm-up run
     sess.run(conv.op)
+
+    # Benchmark run
     t = time.time()
     for _ in range(iterations):
         sess.run(conv.op)
