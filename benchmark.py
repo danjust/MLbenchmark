4eb699183fd64e6c8db44be366ff045c4df6ba27
danjust <daju85@gmail.com>
2018-01-05 11:37:14 +0000

    include deep cnn

--- benchmark.py
+++ benchmark.py
@@ -3,15 +3,17 @@ Using TensorFlow
 """
 
 import tensorflow as tf
-from utils_tf.benchmark_matmul import benchmark_matmul
-from utils_tf.benchmark_conv import benchmark_conv
-from utils_tf.benchmark_rnn import benchmark_rnn
+from  utils_tf import benchmark_matmul
+from  utils_tf import benchmark_conv
+from  utils_tf import benchmark_rnn
+from  utils_tf import benchmark_cnn
 
 
 # Benchmarks to perform
 tf.app.flags.DEFINE_bool('testMatMul', True, 'Benchmark matrix multiplication')
 tf.app.flags.DEFINE_bool('testConv', True, 'Benchmark 2D convolution')
-tf.app.flags.DEFINE_bool('testRNN', True, 'Benchmark recurrent neural networks')
+tf.app.flags.DEFINE_bool('testRNN', False, 'Benchmark recurrent neural networks')
+tf.app.flags.DEFINE_bool('testCNN', False, 'Benchmark a cnn training on cifar10')
 
 # General parameters
 tf.app.flags.DEFINE_string('devlist', '/gpu:0', 'List of device names')
@@ -25,13 +27,18 @@ tf.app.flags.DEFINE_integer('kernelsize', 15, 'Size of kernel for benchmarking c
 # Parameters for RNNs
 tf.app.flags.DEFINE_string('rnn_type', 'rnn', 'Type of RNN (rnn or lstm)')
 tf.app.flags.DEFINE_integer('seq_length', 50, 'Length of sequence')
-tf.app.flags.DEFINE_integer('batch_size', 50, 'Batch size')
+tf.app.flags.DEFINE_integer('batch_size_rnn', 64, 'Batch size')
 tf.app.flags.DEFINE_integer('num_samples', 10000, 'Total number of samples of length seq_length')
 tf.app.flags.DEFINE_integer('num_units', 32, 'Number of hidden units')
 tf.app.flags.DEFINE_integer('num_classes', 10, 'Number of target classes')
 tf.app.flags.DEFINE_integer('learning_rate', 0.001, 'Learning rate')
 tf.app.flags.DEFINE_integer('iter_rnn', 10, 'Number of iterations for RNNs')
 
+# Parameters for CNNs
+tf.app.flags.DEFINE_integer('steps_cnn', 100, 'Number of steps to train CNN')
+tf.app.flags.DEFINE_integer('batch_size_cnn', 128, 'Number of steps to train CNN')
+tf.app.flags.DEFINE_string('data_dir', '/home/djustus/Data/cifar10', 'Directory with training data')
+tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10logs', 'Directory with training logs')
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -42,7 +49,7 @@ def main(_):
                 + (FLAGS.matsize-1)*FLAGS.matsize**2)
                 # matsize**3 multiplications,
                 # (matsize-1)*matsize**2 additions
-        timeUsed = benchmark_matmul(
+        timeUsed = benchmark_matmul.benchmark_matmul(
                 FLAGS.matsize,
                 FLAGS.iter,
                 FLAGS.devlist,
@@ -59,7 +66,7 @@ def main(_):
                 * (FLAGS.kernelsize**3
                 + (FLAGS.kernelsize-1)*FLAGS.kernelsize**2))
                 # (matsize.kernelsize+1)**2 GEMMs
-        timeUsed = benchmark_conv(
+        timeUsed = benchmark_conv.benchmark_conv(
                 FLAGS.matsize,
                 FLAGS.kernelsize,
                 FLAGS.iter,
@@ -73,10 +80,10 @@ def main(_):
                 1/timeUsed))
 
     if FLAGS.testRNN:
-        timeUsed = benchmark_rnn(
+        timeUsed = benchmark_rnn.benchmark_rnn(
                 FLAGS.rnn_type,
                 FLAGS.seq_length,
-                FLAGS.batch_size,
+                FLAGS.batch_size_rnn,
                 FLAGS.num_samples,
                 FLAGS.num_units,
                 FLAGS.num_classes,
@@ -87,6 +94,17 @@ def main(_):
         print("\n%s:  %.2f steps per sec"
                 % (FLAGS.rnn_type,
                 1/timeUsed))
+
+    if FLAGS.testCNN:
+        timeUsed = benchmark_cnn.train(
+                FLAGS.data_dir,
+                FLAGS.train_dir,
+                FLAGS.batch_size_cnn,
+                FLAGS.steps_cnn)
+        print("convolutional neural network, %d training steps:  %.2f steps per sec (%2.f images per sec)"
+                % (FLAGS.steps_cnn,
+                FLAGS.steps_cnn/timeUsed,
+                FLAGS.steps_cnn*FLAGS.batch_size_cnn/timeUsed))
 
 if __name__ == '__main__':
   tf.app.run()
