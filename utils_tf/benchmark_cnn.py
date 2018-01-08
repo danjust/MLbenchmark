7563d91cf2b0994dcdc32b30c0c771c27f68d001
danjust <daju85@gmail.com>
2018-01-08 12:28:22 +0000

    no logging

--- utils_tf/benchmark_cnn.py
+++ utils_tf/benchmark_cnn.py
@@ -21,18 +21,17 @@ def train(data_dir,batch_size,max_steps,num_gpu,devlist):
 
     with tf.Graph().as_default():
         global_step = tf.train.get_or_create_global_step()
-
         # Get images and labels for CIFAR-10.
         # Force input pipeline to CPU:0 to avoid operations sometimes
         # ending up on GPU and resulting in a slow down.
+        with tf.device('/cpu:0'):
+            binary_dir = os.path.join(data_dir, 'cifar-10-batches-bin')
+            images, labels = cifar10_input.inputs(eval_data=False,
+                                                data_dir=binary_dir,
+                                                batch_size=batch_size)
+
         for dev in devlist:
             with tf.device(dev):
-                binary_dir = os.path.join(data_dir, 'cifar-10-batches-bin')
-                images, labels = cifar10_input.inputs(eval_data=False,
-                                                    data_dir=binary_dir,
-                                                    batch_size=batch_size)
-
-
                 # Build a Graph that computes the logits predictions from the
                 # inference model.
                 logits = cifar10.inference(images,batch_size)
@@ -44,13 +43,19 @@ def train(data_dir,batch_size,max_steps,num_gpu,devlist):
                 # updates the model parameters.
                 train_op = cifar10.train(loss, global_step,batch_size)
 
-        with tf.train.MonitoredTrainingSession() as mon_sess:
-            print("========================================\n")
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            coord = tf.train.Coordinator()
+            tf.train.start_queue_runners(sess=sess,coord=coord)
+
+            threads = print("========================================\n")
             print("start training cnn")
             tstart = time.time()
             for steps in range(max_steps):
-                mon_sess.run(train_op)
+                sess.run(train_op)
             tstop = time.time()
             dur = tstop-tstart
             print("========================================\n")
+            coord.request_stop()
+            coord.join(threads)
     return dur
