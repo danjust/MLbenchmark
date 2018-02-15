d966398e0fa88d559c7c3b9671c9a587ba7589db
danjust <daju85@gmail.com>
2018-02-15 15:32:23 +0000

    hard code layers for testing

--- utils_tf/utils_cnn/cnn_multidevicev3.py
+++ utils_tf/utils_cnn/cnn_multidevicev3.py
@@ -19,14 +19,15 @@ def build_model(
 
     # Conv layer 0
     with tf.variable_scope('conv_0') as scope:
-        kernel0 = tf.get_variable(
-                'weights_0',
-                shape=[kernel_size[0], kernel_size[0], num_channels, num_features[0]],
-                initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
-        biases0 = tf.get_variable(
-                'biases_0',
-                shape=[num_features[0]],
-                initializer=tf.constant_initializer(0.0))
+        with tf.device('/cpu:0'):
+            kernel0 = tf.get_variable(
+                    'weights_0',
+                    shape=[kernel_size[0], kernel_size[0], num_channels, num_features[0]],
+                    initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
+            biases0 = tf.get_variable(
+                    'biases_0',
+                    shape=[num_features[0]],
+                    initializer=tf.constant_initializer(0.0))
         conv0 = tf.nn.conv2d(
                 input=images,
                 filter=kernel0,
@@ -45,14 +46,15 @@ def build_model(
 
     # Conv layer 1
     with tf.variable_scope('conv_1') as scope:
-        kernel1 = tf.get_variable(
-                'weights_1',
-                shape=[kernel_size[1], kernel_size[1], num_features[0], num_features[1]],
-                initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
-        biases1 = tf.get_variable(
-                'biases_1',
-                shape=[num_features[1]],
-                initializer=tf.constant_initializer(0.0))
+        with tf.device('/cpu:0'):
+            kernel1 = tf.get_variable(
+                    'weights_1',
+                    shape=[kernel_size[1], kernel_size[1], num_features[0], num_features[1]],
+                    initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
+            biases1 = tf.get_variable(
+                    'biases_1',
+                    shape=[num_features[1]],
+                    initializer=tf.constant_initializer(0.0))
         conv1 = tf.nn.conv2d(
                 input=pool0,
                 filter=kernel1,
@@ -71,14 +73,15 @@ def build_model(
 
     # Conv layer 2
     with tf.variable_scope('conv_2') as scope:
-        kernel2 = tf.get_variable(
-                'weights_2',
-                shape=[kernel_size[2], kernel_size[2], num_features[1], num_features[2]],
-                initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
-        biases2 = tf.get_variable(
-                'biases_2',
-                shape=[num_features[2]],
-                initializer=tf.constant_initializer(0.0))
+        with tf.device('/cpu:0'):
+            kernel2 = tf.get_variable(
+                    'weights_2',
+                    shape=[kernel_size[2], kernel_size[2], num_features[1], num_features[2]],
+                    initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
+            biases2 = tf.get_variable(
+                    'biases_2',
+                    shape=[num_features[2]],
+                    initializer=tf.constant_initializer(0.0))
         conv2 = tf.nn.conv2d(
                 input=pool1,
                 filter=kernel2,
@@ -102,14 +105,15 @@ def build_model(
 
     dim = pool_flat.get_shape()[1].value
     with tf.variable_scope('dense') as scope:
-        weightsd = tf.get_variable(
-                'weights_dense',
-                shape=[dim, fully_connected_size],
-                initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
-        biasesd = tf.get_variable(
-                'biases_dense',
-                shape=[fully_connected_size],
-                initializer=tf.constant_initializer(0.1))
+        with tf.device('/cpu:0'):
+            weightsd = tf.get_variable(
+                    'weights_dense',
+                    shape=[dim, fully_connected_size],
+                    initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
+            biasesd = tf.get_variable(
+                    'biases_dense',
+                    shape=[fully_connected_size],
+                    initializer=tf.constant_initializer(0.1))
         dense = tf.nn.relu(tf.matmul(pool_flat, weightsd) + biasesd, name=scope.name)
 
 
@@ -119,14 +123,15 @@ def build_model(
             training=True)
 
     with tf.variable_scope('softmax') as scope:
-        weightss = tf.get_variable(
-                'weights_logits',
-                shape=[fully_connected_size, num_classes],
-                initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
-        biasess = tf.get_variable(
-                'biases_logits',
-                shape=[num_classes],
-                initializer=tf.constant_initializer(0.1))
+        with tf.device('/cpu:0'):
+            weightss = tf.get_variable(
+                    'weights_logits',
+                    shape=[fully_connected_size, num_classes],
+                    initializer=tf.truncated_normal_initializer(stddev=5e-3, dtype=tf.float32))
+            biasess = tf.get_variable(
+                    'biases_logits',
+                    shape=[num_classes],
+                    initializer=tf.constant_initializer(0.1))
         logits = tf.add(tf.matmul(dropout, weightss), biasess, name=scope.name)
 
 
