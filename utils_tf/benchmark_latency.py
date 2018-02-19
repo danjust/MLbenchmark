aa2ff04a3164401a46d492b70090dced42393036
danjust <daju85@gmail.com>
2018-02-19 14:55:41 +0000

    benchmark latency

--- utils_tf/benchmark_latency.py
+++ utils_tf/benchmark_latency.py
@@ -5,9 +5,9 @@ import time
 
 def benchmark_latency(matsize,iterations,device):
     with tf.device('/cpu:0'):
-        mat = tf.Variable(tf.ones(matsize,matsize))
+        mat = tf.Variable(tf.ones([matsize,matsize],dtype=tf.float32))
     with tf.device(device):
-        mat_device = tf.indentify(mat)
+        mat_device = tf.identity(mat)
 
     # Creates the session
     config = tf.ConfigProto(
